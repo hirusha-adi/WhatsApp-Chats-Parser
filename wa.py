41e666ae315f2d869d1d0c7ad9ea12a4f5f0d9d4
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-11-13 14:37:30 +0530

    add static typing

--- wa.py
+++ wa.py
@@ -2,9 +2,10 @@ import re
 import argparse
 import json
 import csv
+from typing import List, Dict, Union
 
 
-def parse_whatsapp_chat(chat_text):
+def parse_whatsapp_chat(chat_text: str) -> List[Dict[str, Union[str, bool]]]:
     messages = []
 
     group_creation_pattern = re.compile(
@@ -21,9 +22,7 @@ def parse_whatsapp_chat(chat_text):
     for line in lines:
         group_creation_match = group_creation_pattern.match(line)
         if group_creation_match:
-            timestamp = group_creation_match.group(1)
-            creator = group_creation_match.group(2)
-            group_name = group_creation_match.group(3)
+            timestamp, creator, group_name = group_creation_match.groups()
             messages.append({
                 'timestamp': timestamp,
                 'creator': creator,
@@ -32,9 +31,7 @@ def parse_whatsapp_chat(chat_text):
         else:
             member_addition_match = member_addition_pattern.match(line)
             if member_addition_match:
-                timestamp = member_addition_match.group(1)
-                user = member_addition_match.group(2)
-                added_member = member_addition_match.group(3)
+                timestamp, user, added_member = member_addition_match.groups()
                 messages.append({
                     'timestamp': timestamp,
                     'user': user,
@@ -43,9 +40,7 @@ def parse_whatsapp_chat(chat_text):
             else:
                 attachment_match = attachment_pattern.match(line)
                 if attachment_match:
-                    timestamp = attachment_match.group(1)
-                    sender = attachment_match.group(2)
-                    message = attachment_match.group(3)
+                    timestamp, sender, message, _ = attachment_match.groups()
                     messages.append({
                         'timestamp': timestamp,
                         'sender': sender,
@@ -55,9 +50,7 @@ def parse_whatsapp_chat(chat_text):
                 else:
                     match = pattern.match(line)
                     if match:
-                        timestamp = match.group(1)
-                        sender = match.group(2)
-                        message = match.group(3)
+                        timestamp, sender, message = match.groups()
                         messages.append({
                             'timestamp': timestamp,
                             'sender': sender,
@@ -68,12 +61,12 @@ def parse_whatsapp_chat(chat_text):
     return messages
 
 
-def save_to_json(messages, output_file):
+def save_to_json(messages: List[Dict[str, Union[str, bool]]], output_file: str) -> None:
     with open(output_file, 'w') as file:
         json.dump(messages, file, indent=4)
 
 
-def save_to_csv(messages, output_file):
+def save_to_csv(messages: List[Dict[str, Union[str, bool]]], output_file: str) -> None:
     keys = messages[0].keys() if messages else []
     with open(output_file, 'w', newline='') as output_file:
         writer = csv.DictWriter(output_file, fieldnames=keys)
@@ -81,7 +74,7 @@ def save_to_csv(messages, output_file):
         writer.writerows(messages)
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(description='WhatsApp Chat Parser')
     parser.add_argument('input_file', help='Input file name')
     parser.add_argument('--json', dest='json', nargs='?',
